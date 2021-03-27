c8f656abf6e5cc8c08b2aaf5043e47ac43d6ed2d
scott <scottajevans@gmail.com>
2021-03-27 16:02:09 +0000

    Making it display one at a time

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
+import React, {Component} from 'react';
+
 import './App.css';
 import {Game} from './components/game.js';
+
 let games = require('./games.json');
 
-function App() {
-  console.log(games)
-  const gamesDisplay = Object.values(games).map((game, index) => {
-    return (<li key={index}>
-      <Game game={game}></Game>
-    </li>)
-  });
+class App extends Component {
+  constructor(){
+    super();
 
-  return (
+    games.sort(() => 0.5 - Math.random());
+
+    this.state = {
+      gamePos : 0,
+      currentGame : games[0],
+      games : games
+    }
+  }
+
+  gamesDisplay = () => {
+    return <Game game={this.state.currentGame}></Game>
+  }
+
+  clickedButton = () => {
+    let nextPos = this.state.gamePos === games.length - 1 ? 0 : this.state.gamePos + 1;
+    this.setState({
+      gamePos : nextPos,
+      currentGame : games[nextPos]
+    })
+  }
+
+  render() {
+    return (
     <div className="App">
       <header className="App-header">
         <h1>S.I.B.M.</h1>
         <h4>For Super Import Business Meeting use only</h4>
-        <ul>
-          {gamesDisplay}
-        </ul>
+        {this.gamesDisplay()}
+        <div>
+          <button onClick={this.clickedButton}>Next</button>
+        </div>
       </header>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/game.js
+++ src/components/game.js
@@ -1,10 +1,26 @@
 import React from 'react';
 
 export class Game extends React.Component {
+
+    getTags = () => {
+        console.log("Hello")
+        console.log(this.props.game.tags)
+        return this.props.game.tags.map((tag) => {
+            return <>{tag} </>
+        })
+    }
+
     render() {
         return (
             <div>
-                <a href={this.props.game.url}>{this.props.game.name}</a>
+                {this.props.game.name}
+                <div>
+                    <a href={this.props.game.url}>
+                        <img src={this.props.game.image} className="game-img"></img>
+                    </a>
+                </div>
+                <div>What is this game?</div>
+                <div><p>{this.getTags()}</p></div>
             </div>
         );
     }
