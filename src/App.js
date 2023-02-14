1acae841a89a6ac705ab5367190cb22d7d6b214a
Scott Evans <scott@Scotts-MacBook-Pro.local>
2023-02-14 17:32:04 +0000

    Change the url to just data as fetch works differently

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ export const App = () => {
   let exitDelay = 400;
 
   useEffect(() => {
-    fetch('https:scottajevans.github.io/sibmdata/data.json')
+    fetch('data.json')
       .then(response => response.json())
       .then(data => {
         data.sort(() => 0.5 - Math.random());
